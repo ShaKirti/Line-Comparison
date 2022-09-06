8b379ff4f3a6701e4320af809d6097b05ff6287b
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-09-06 19:29:26 +0530

    Added: Length of a line logic

--- src/com/blz/linecomparison/LineComparison.java
+++ src/com/blz/linecomparison/LineComparison.java
@@ -3,5 +3,16 @@ package com.blz.linecomparison;
 public class LineComparison {
     public static void main(String args[]){
         System.out.println("Welcome to Line Comparison Computation Program");
+
+        // Point 1
+        int x1 = 15;
+        int y1 = 2;
+
+        // Point 2
+        int x2 = 17;
+        int y2 = 13;
+
+        double length = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println(length);
     }
 }
