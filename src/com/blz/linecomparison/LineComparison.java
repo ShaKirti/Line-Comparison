e1900d3547bb02705a43ce90e07e21874f7f6332
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-09-07 10:32:07 +0530

    Added: Comparison of lengths of two lines.

--- src/com/blz/linecomparison/LineComparison.java
+++ src/com/blz/linecomparison/LineComparison.java
@@ -12,7 +12,22 @@ public class LineComparison {
         int x2 = 17;
         int y2 = 13;
 
-        double length = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-        System.out.println(length);
+        // Point 3
+        int x3 = 9;
+        int y3 = 8;
+
+        // point 4
+        int x4 = 14;
+        int y4 = 16;
+
+        double length1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println(length1);
+        double length2 = Math.sqrt((x4-x3)*(x4-x3) + (y4-y3)*(y4-y3));
+        System.out.println(length2);
+
+        if (length1 == length2)
+            System.out.println("Both lines are of same length.");
+        else
+            System.out.println("Both lines are of different length.");
     }
 }
